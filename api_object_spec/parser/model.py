0d4a031e88a88b15354d3ca45ed10d170db78da6
AWinterman <andywinterman@gmail.com>
2015-04-23 12:20:40 -0700

    got it working after merge

--- api_object_spec/parser/model.py
+++ api_object_spec/parser/model.py
@@ -8,6 +8,7 @@ Definition = namedtuple('Definition', ['constraints', 'name'])
 
 # IR nodes and such
 
+
 class Constraint(object):
     __metaclass__ = abc.ABCMeta
 
@@ -33,6 +34,7 @@ class Constraint(object):
                 bool: BooleanConstraint(obj),
                 None: NullConstraint()
             }[type(obj)]
+
 
 class RefConstraint(Constraint):
     def __init__(self, name):
@@ -61,45 +63,56 @@ class ObjectConstraint(Constraint):
                 return False
         return True
 
+
 class ObjectRefConstraint(RefConstraint):
     def __init__(self, name):
         RefConstraint.__init__(self, name)
+
     def match(self, data):
         return isinstance(data, dict)
-
-class KeyValueConstraint(Constraint):
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-    def reify(self):
-        return (self.key.reify(), self.value.reify())
-    def match(self, data):
-        if self.key.match(data):
-            # gross
-            if self.value.match(data[self.key.key]):
-                return True
-        return False
 
 class KeyConstraint(Constraint):
     def __init__(self, key):
         self.key = key
+
     def reify(self):
         return self.key
+
     def match(self, data):
         return self.key in data
 
-#  broken until arrays are real
+class ArrayElementConstraint(Constraint):
+    def __init__(self, constraint, index):
+        self.constraint = constraint
+        self.index = index
+
+    def match(self, array):
+        return self.constraint.match(array[self.index])
+
+    def reify(self):
+        return self.constraint.reify()
+
+
 class ArrayConstraint(Constraint):
-    def __init__(self, elements):
-        self.elements = []
-        for element in elements:
-            self.elements.append(Constraint.wrap(element))
+    def __init__(self, constraints):
+        self.constraints = constraints
 
     def reify(self):
         output = []
-        for element in self.elements:
+        for element in self.constraints:
             output = element.reify()
         return output
+
+    def match(self, data):
+        if not isinstance(data, list):
+            return False
+
+        for element in self.constraints:
+            if not element.match(data):
+                return False
+
+        return True
+
 
 class StringConstraint(Constraint):
     def __init__(self, string):
@@ -168,18 +181,44 @@ class TokenConstraint(Constraint):
 class RepeatedTokenConstraint(Constraint):
     def __init__(self, name):
         self.name = name
+
     def reify(self):
         # todo, reify as something more appropriate
         return "<" + self.name + ">" + "..."
+
     def match(self, data):
         # not yet
-        return false
+        return False
+
+class KeyValueConstraint(Constraint):
+    def __init__(self, constraint):
+        # TODO: validate that the constraint is either a pair or a token or repeated token which maps to a pairs
+        self.constraint = constraint
+
+    def reify(self):
+        return self.constraint.reify()
+
+    def match(self, data):
+        return self.constraint.match(data)
+
+class PairConstraint():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def reify(self):
+        kr = self.key.reify()
+        vr = self.value.reify()
+        return kr, vr
+
+    def match(self, data):
+        if self.key.match(data):
+            # gross
+            if self.value.match(data[self.key.key]):
+                return True
+        return False
 
 class ConstraintModel(object):
-    """
-    Instantiates a callable which takes text as input and returns a constraint tree as output.
-    """
-
     def __init__(self, jsl):
         grammar_model = self.grammar_model(jsl)
         self.definitions = {}
@@ -219,31 +258,55 @@ class ConstraintModel(object):
         return model
 
     def _definition(self, node):
+        kv = node.pair
+        val = node.value
 
-        for key_value in node.key_value:
-            constraints = self._key_value(key_value)
-
-        for value in node.value:
-            constraints = self._value(value)
+        if kv:
+            constraints = self._pair(*kv)
+        elif val:
+            constraints = self._value(*val)
+        else:
+            raise ValueError("{} is not a valid definition body".format(node))
 
         return Definition(name=node.descend('name')[0].text, constraints=constraints)
 
-    def _array(node):
-        # TODO: actually write this bad boy.
-        return []
+    def _array(self, array):
+        constraints = []
+        elements = array.element
+        # for some reason nodes come out in reverse order, hence:
+        elements.reverse()
+
+        for index, element in enumerate(elements):
+            value = element.value
+            token = element.token
+
+            if value:
+                el = self._value(*value)
+            elif token:
+                el = self._token(*token)
+            else:
+                raise ValueError('"{}" is an illegal element'.format(element))
+
+            constraints.append(ArrayElementConstraint(el, index))
+
+        return ArrayConstraint(constraints)
 
     def _value(self, val):
+        obj = val.object
+        array = val.array
+        primitive = val.primitive
 
-        for obj in val.object:
-            constraints = self._object(obj)
+        # The following calls use argument unpacking, so that they throw if you somehow have too many elements here.
+        if obj:
+            return self._object(*obj)
 
-        for array in val.array:
-            constraints = self._array(array)
+        if array:
+            return self._array(*array)
 
-        for primitive in val.primitive:
-            constraints = self._primitive(primitive)
+        if primitive:
+            return self._primitive(*primitive)
 
-        return constraints
+        raise ValueError('"{}" is not a value'.format(val))
 
     def _object(self, obj):
 
@@ -254,16 +317,41 @@ class ConstraintModel(object):
 
         return ObjectConstraint(pairs)
 
+
     def _pair(self, pair):
+        token = pair.token
+        key = pair.pair_key
+        value = pair.pair_value
 
-        # not sure how to handle this one yet?
-        for token in pair.token:
-            constraints = self._token(token)
+        # using unpacking here, should only get one element, if you have more it will throw.
+        if token:
+            return KeyValueConstraint(self._token(*token))
+        elif key and value:
+            return KeyValueConstraint(PairConstraint(self._pair_key(*key), self._pair_value(*value)))
+        else:
+            raise ValueError('"{}" is not a pair'.format(pair))
 
-        for kv in pair.key_value:
-            constraints = self._key_value(kv)
+    def _pair_key(self, key):
+        token = key.one_token
+        string = key.string
 
-        return constraints
+        if token:
+            return self._one_token(*token)
+        elif string:
+            return self._string(*string)
+        else:
+            raise ValueError('node "{}" must either be a string or a single token'.format(key))
+
+    def _pair_value(self, node):
+        value = node.value
+        token = node.one_token
+
+        if token:
+            return self._one_token(*token)
+        elif value:
+            return self._value(*value)
+        else:
+            raise ValueError('"{}" must be a JSON value or a single token'.format(value))
 
     def _key_value(self, node):
 
@@ -291,6 +379,7 @@ class ConstraintModel(object):
 
     def _one_token(self, token):
         name = token.descend('token_text')[0].text
+
         return TokenConstraint(name, self.definitions)
 
     def _repeated_token(self, token):
@@ -303,7 +392,7 @@ class ConstraintModel(object):
         value = n.text
 
         if n.string:
-            return StringConstraint(n.text.strip('"'))
+            return self._string(n)
         elif n.number:
             return NumberConstraint(float(n.text))
         elif n.boolean:
@@ -314,6 +403,10 @@ class ConstraintModel(object):
         else:
             raise ValueError('{} is not a primitive'.format(n))
 
+    @staticmethod
+    def _string(n):
+        return StringConstraint(n.text.strip('"'))
+
 #####
 
 sampleJSL = """
@@ -321,6 +414,7 @@ sampleJSL = """
 wow = "such":"pair"
 name = "randy"
 hyderabad = {"anynumber": <number>}
+things = ["foo", "barf", "dear friends"]
 red = {"subjective":true, "rgb":"1 0 0"}
 foo = {"color":<red>, "doge":<wow>, "bar":<hyderabad>, "my name is":<name>, "neat":12.59199, "cool":{"yay":"radical"}}
 
@@ -330,3 +424,7 @@ model = ConstraintModel(sampleJSL)
 
 print(model.generate("foo"))
 print(model.validate("foo", {"color":{"subjective":True, "rgb":"1 0 0"}, "doge":{"such":"pair"}, "bar":{"anynumber":42}, "my name is":"randy", "neat":12.59199, "cool":{"yay":"radical"}}))
+
+print(model.generate('things'))
+print model.validate('things', ['food', 'barf', 'dear friends'])
+print model.generate('wow')
