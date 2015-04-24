e11e72620cd2e16ec6f2da5fd3a201c7dc599482
AWinterman <andywinterman@gmail.com>
2015-04-24 09:47:12 -0700

    denest

--- api_object_spec/__init__.py
+++ api_object_spec/__init__.py
@@ -1,8 +1,1 @@
-"""
-api_object_spec.parser
-======================
-
-Creates model objects from raw text input.
-
-See :ref:`specification-format` for the expected text input format.
-"""
+__author__ = 'andrewwinterman'
